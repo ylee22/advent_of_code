77f10ec6a388844e75aabf10cb861fda0144d2f3
Yerim Lee <rimmy2002@gmail.com>
2022-04-22 14:45:15 -0700

    finished day 4 challenge

--- day4/rust/src/main.rs
+++ day4/rust/src/main.rs
@@ -1,70 +1,90 @@
 use std::io;
 use std::io::prelude::*;
 use std::collections::HashMap;
+use std::iter::FromIterator;
 
-// struct BoardInfo {
-    // board: Vec<Vec<i32>>,
-    //board: HashMap<i32, (i8, i8)>,
-    //board_theotherway: HashMap<(i8, i8), i32>,
-    
-    //board: Vec<i32>
-    // row_matches: Vec<usize>,
-    // column_matches: Vec<usize>
-// }
+#[derive(Debug,PartialEq)]
+struct Board{
+    cells: HashMap<Value, Cell>,
+    won: i32,
+}
 
-// impl BoardInfo {
-    // fn get(&self, row: usize, col: usize) -> i32 {
-    //     //self.board[row * 5 + col]
-    //     if(row1 >> 8 & 0xFF)
-    // }
+type Value = i32;
 
-    // fn all_nums(&self) -> &[i32] {
+#[derive(Debug,PartialEq)]
+struct Cell {
+    row: i32,
+    col: i32,
+    mark: bool,
+}
 
-    // }
+impl Cell {
+    fn new(row: i32, col: i32) -> Self {
+        return Cell{row, col, mark:false}
+    }
+}
 
-//     fn get_loc(&self, n: i32) -> Option<(usize, usize)> {
-//         for idx in 0 .. self.board.len() {
-//             if self.board[idx] == n {
-//                 return Some((idx / 5, idx % 5));
-//             }
-//         }
+impl Board {
+    fn new(v: Vec<(i32,i32,i32)>) -> Self {
+        let spaces = v.iter().map(|(space, r, c)| (*space, Cell::new(*r, *c)));
+        let hash : HashMap<i32, Cell> = HashMap::from_iter(spaces);
 
-//         return None;
-//     }
-// }
+        // let board = HashMap::new();
+        // for (space, r, c) in v {
+        //     board.insert(space, Cell::new(r,c));
+        // }
+        
+        return Board{cells: hash, won: 0}
+    }
 
-// fn check_row(bi: &BoardInfo, row: usize, nums: &[i32]) -> Option<usize> {
-//     let row = &bi.board[row];
-//     let mut hits = 0;
+    fn get_loc(&self, number: i32) -> Option<(i32, i32)> {
+        let cell = self.cells.get(&number);
+        match cell {
+            Some(c) => { Some((c.row, c.col)) }
+            None => { None }
+        }
+    }
 
-//     for n in nums {
-//         todo!()
-//     }
+    fn update(&mut self, n: i32) {
+        if self.cells.contains_key(&n) {
+            self.cells.get_mut(&n).unwrap().mark = true;
+        }
+    }
 
-//     todo!()
-// }
+    fn bingo(&self) -> bool {
+        let mut rs = HashMap::from([(0,0),(1,0),(2,0),(3,0),(4,0)]);
+        let mut cs = HashMap::from([(0,0),(1,0),(2,0),(3,0),(4,0)]);
+        for cell in self.cells.values() {
+            if cell.mark == true {
+                let r = rs.get_mut(&cell.row).unwrap();
+                *r = *r + 1;
+                let c = cs.get_mut(&cell.col).unwrap();
+                *c = *c + 1;
+            }
+        }
+        for count in rs.values() {
+            if count == &5 {
+                return true
+            }
+        }
+        for count in cs.values() {
+            if count == &5 {
+                return true
+            }
+        }
+        return false
+    }
 
-// fn bingo_match(bi: &BoardInfo, num: i32) -> BoardInfo {
-//     let mut board: &Vec<Vec<i32>> = &bi.board;
-//     let mut row_matches: &Vec<usize> = &bi.row_matches;
-//     let mut column_matches: &Vec<usize> = &mut bi.column_matches;
-//     let mut row = 0;
-//     for line in &bi.board {
-//         let column = line.iter().position(|&x| x==num);
-//         match column {
-//             Some(c) => {bi.column_matches.push(c);
-//                               bi.row_matches.push(row)
-//                             }
-//             _ => continue,
-//         }
-//         row = row + 1;
-//     }
-
-//     bi.row_matches = row_matches;
-//     bi.column_matches = column_matches;
-    
-//     return BoardInfo { board: , row_matches: (), column_matches: () };
-// }
+    fn score (&self, n: i32) -> i32 {
+        let mut s = 0;
+        for (num, cell) in self.cells.iter() {
+            if cell.mark == false {
+                s = s + num
+            }
+        }
+        return s * n
+    }
+}
 
 fn parse_numbers(line: &String) -> Vec<i32> {
     let split_line = line.split(",").map(|s| s.parse::<i32>().unwrap());
@@ -72,73 +92,35 @@ fn parse_numbers(line: &String) -> Vec<i32> {
     return nums;
 }
 
-fn parse_board_line(line: &String, b: &mut HashMap<i32,(i32,i32,bool)>, row: i32) {
+fn parse_board_line(line: &String, b: &mut Board, row: i32) {
     let nums = line.split_whitespace().map(|s| s.parse::<i32>().unwrap());
     let mut col = 0;
     for num in nums {
         // todo!("duplicate numbers in the board")
-        b.insert(num, (row, col, false));
+        b.cells.insert(num, Cell::new(row, col));
         col = col + 1;
     }
 }
 
-fn update_board(b: &mut HashMap<i32,(i32,i32,bool)>, num_drawn: i32) {
-    if b.contains_key(&num_drawn) {
-        let (_,_,f) = b.get_mut(&num_drawn).unwrap();
-        *f = true
-        // why does the following not work?
-        // let f = true;
-        // f = true
-        // let *f = true;
-    } 
-}
+fn assemble_boards<T: Iterator<Item=String>>(iter_lines: T) -> Vec<Board> {
+    let mut boards: Vec<Board> = Vec::new();
+   
+    let mut lns = iter_lines.filter(|l| !l.is_empty());
 
-fn test(b: &HashMap<i32,(i32,i32,bool)>) -> (HashMap<i32,i32>, HashMap<i32,i32>) {
-    let mut rm = HashMap::from([(0,0),(1,0),(2,0),(3,0),(4,0)]);
-    let mut cm = HashMap::from([(0,0),(1,0),(2,0),(3,0),(4,0)]);
-    for (r, c, m) in b.values() {
-        if m == &true {
-            let ri = rm.get_mut(r).unwrap();
-            *ri = *ri + 1;
-            let ci = cm.get_mut(c).unwrap();
-            *ci = *ci + 1;
+    loop {
+        let first_line = 
+            match lns.next() {
+                None => return boards,
+                Some(line) => line,
+            };
+        
+        let mut board = Board{ cells: HashMap::new(), won: 0 };
+        parse_board_line(&first_line, &mut board, 0);
+        for i in 1..5 {
+            parse_board_line(&lns.next().unwrap(), &mut board, i);
         }
+        boards.push(board);
     }
-    return (rm,cm);
-}
-
-fn bingo(b: &HashMap<i32,(i32,i32,bool)>) -> bool {
-    let mut rm = HashMap::from([(0,0),(1,0),(2,0),(3,0),(4,0)]);
-    let mut cm = HashMap::from([(0,0),(1,0),(2,0),(3,0),(4,0)]);
-    for (r, c, m) in b.values() {
-        if m == &true {
-            let ri = rm.get_mut(r).unwrap();
-            *ri = *ri + 1; // THIS DOESN'T UPDATE PAST 1!!! WHY!!!!
-            let ci = cm.get_mut(c).unwrap();
-            *ci = *ci + 1; // THIS DOESN'T UPDATE PAST 1!!! WHY!!!!
-        }
-    }
-    for v in rm.values() {
-        if v == &5 {
-            return true
-        }
-    }
-    for v in cm.values() {
-        if v == &5 {
-            return true
-        }
-    }
-    return false;
-}
-
-fn score(b: &HashMap<i32,(i32,i32,bool)>) -> i32 {
-    let mut s = 0;
-    for (n, (_,_,m)) in b {
-        if m == &true {
-            s = s + n;
-        }
-    }
-    return s;
 }
 
 fn main() {
@@ -148,42 +130,33 @@ fn main() {
 
     let numbers = parse_numbers(&iter_lines.next().unwrap());
 
-    // TODO: use Vec<HashMap> instead of HashMap<Hashmap>
-    // TODO: use struct to name the inner HashMap as board (abstraction)
-    // - use impl on a struct to write methods
-    // board cell: (i32,i32,bool)
-    // mark board or something as the method name for updating the bool
-    // TODO: make a board and cell abstraction
-    let mut boards: HashMap<i32, HashMap<i32,(i32,i32,bool)>> = HashMap::new();
-    let mut board_counter = 0;
-
     // assemble board
-    for line in iter_lines {
-        // TODO: blank line
-        let mut board = HashMap::new();
-        // TODO: detect board size (check the length of the first line)
-        for row in 0..5 {
-            parse_board_line(&line, &mut board, row)
-        }
-        boards.insert(board_counter, board);
-        board_counter = board_counter + 1;
-    }
+    let mut boards = assemble_boards(iter_lines);
 
-    // // get bingo
-    // for n in numbers {
-    //     for b in boards.values_mut() {
-    //         update_board(b, n);
-    //         let t = bingo(b);
-    //         if t {
-    //             let s = score(b);
-    //             // and then return the score somehow
-    //         }
-    //     }
-    // }
+    // get bingo
+    let mut won: i32 = 0;
+    for n in numbers {
+        for (i, b) in boards.iter_mut().enumerate() {
+            b.update(n);
+            let t = b.bingo();
+            if t & (b.won == 0){
+                won = won + 1;
+                // if won, mark board as won
+                b.won = won;
+                let s = b.score(n);
+                
+                println!("{}: {}",i,s);
+                // panic!("How do you exit gracefully?")
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
+    use crate::{Cell, Board, assemble_boards};
 
     #[test]
     fn parse_numbers_correct() {
@@ -193,32 +166,70 @@ mod tests {
 
     #[test]
     fn parse_board_line_correct() {
-        let mut b = std::collections::HashMap::<i32, (i32,i32,bool)>::new();
-        crate::parse_board_line(&"22 13 17 11  0".to_string(), &mut b, 0);
-        let f = std::collections::HashMap::from([(22,(0,0,false)),(13,(0,1,false)),(17,(0,2,false)),(11,(0,3,false)),(0,(0,4,false))]);
-        assert_eq!(b, f);
+        let correct_board = crate::Board::new(vec![
+            (22,0,0),(13,0,1),(17,0,2),(11,0,3),(0,0,4),
+            (22,1,0),(13,1,1),(17,1,2),(11,1,3),(0,1,4),
+            ]);
+
+        let mut test_board = crate::Board{cells: HashMap::new(), won: 0};
+        crate::parse_board_line(&"22 13 17 11  0".to_string(), &mut test_board, 1);
+        assert_eq!(test_board, correct_board);
     }
 
     #[test]
     fn update_board_correct() {
-        let mut b = std::collections::HashMap::<i32,(i32,i32,bool)>::from([(13,(0,2,false))]);
-        crate::update_board(&mut b, 13);
-        let (_,_,x) = b.get(&13).unwrap();
-        assert_eq!(x,&true);
+        let mut b = crate::Board::new(vec![(13,0,2)]);
+        b.update(13);
+        let x:&Cell = b.cells.get(&13).unwrap();
+        assert_eq!(x.mark,true);
     }
 
     #[test]
-    fn bingo_row() {
-        let b = std::collections::HashMap::<i32,(i32,i32,bool)>::from([
-            (0,(0,0,true)),
-            (1,(1,0,true)),
-            (2,(2,0,true)),
-            (3,(3,0,true)),
-            (4,(4,0,true))]);
-        println!("{:?}", b);
-        let (_,cm) = crate::test(&b);
-        let c = cm.get(&0);
-        assert_eq!(c.unwrap(),&5);
+    fn bingo_col() {
+        let mut b: Board = Board::new(vec![(0,0,0),(1,1,0),(2,2,0),(3,3,0),(4,4,0)]);
+        for v in 0..5 {
+            b.update(v)
+        }
+        let x = b.bingo();
+        assert_eq!(x, true);
     }
 
+    #[test]
+    fn assemble_boards_correct() {
+        let lines = vec!["",
+            "68 73 98 51 49",
+            "82 56 87 64  8",
+            "46  7 21 38 30",
+            "66  5 86 97 74",
+            "60 63 76 55 39",
+            "",
+            "92 20 87 77 52",
+            "72 29 81 24 64",
+            "26 16 19 79 68",
+            "8 53 90 14 74",
+            "28 89 78 54 15",
+            "",
+            "13 17 35  2 85",
+            "37 87 57 74 65",
+            "60 21 18 98 96",
+            "4 51 46 84  0",
+            "90 75 80 41 64",
+            ""];
+        
+        let lines_iter = lines.iter().map(|x| x.to_string());
+
+        let test_boards = assemble_boards(lines_iter);
+        
+        assert_eq!(test_boards[0].get_loc(7), Some((2,1)));
+        assert_eq!(test_boards[1].get_loc(52), Some((0,4)));
+        assert_eq!(test_boards[2].get_loc(-100), None);
+    }
+
+    #[test]
+    fn mark_won() {
+        let mut b: Board = Board::new(vec![(0,0,0),(1,1,0),(2,2,0),(3,3,0),(4,4,0)]);
+        assert_eq!(0, b.won);
+        b.won = 1;
+        assert_eq!(1, b.won)
+    }
 }
