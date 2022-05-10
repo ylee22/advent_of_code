8cbfad4827b33066f6c2edffa460cff143849867
Yerim Lee <rimmy2002@gmail.com>
2022-05-10 13:45:48 -0700

    finished day 3 part 1

--- day3/rust/src/main.rs
+++ day3/rust/src/main.rs
@@ -48,39 +48,54 @@ fn gamma(counts: &Vec<i32>, total: i32) -> Vec<i8> {
     return gamma
 }
 
-fn beta(gamma: &Vec<i8>) -> Vec<i8> {
-    let mut beta = Vec::<i8>::new();
+fn epsilon(gamma: &Vec<i8>) -> Vec<i8> {
+    let mut ep = Vec::<i8>::new();
     for c in gamma {
         match c {
-            1 => beta.push(0),
-            0 => beta.push(1),
+            1 => ep.push(0),
+            0 => ep.push(1),
             _ => panic!("not 0 or 1")
         }
     }
-    return beta
+    return ep
+}
+
+fn bit_vec_to_int(v: &Vec<i8>) -> i32{
+    let mut score: i32 = 0;
+    let vsize = v.len() - 1;
+    for (i, n) in v.iter().enumerate() {
+        let e = (vsize - i) as u32;
+        score = score + (*n as i32)*(2 as i32).pow(e);
+    }
+    return score
 }
 
 fn main() {
     let (mut counts, total) = parse_lines();
     let g = gamma(&mut counts, total);
-    let b = beta(&g);
-    
+    let b = epsilon(&g);
+    let g = bit_vec_to_int(&g);
+    let b = bit_vec_to_int(&b);
+    println!("{}", g*b)
 }
 
 
 #[cfg(test)]
 mod tests {
-    use crate::{count_ones, gamma, beta};
+    use crate::{count_ones, gamma, epsilon, bit_vec_to_int};
 
     #[test]
-    fn test_count_ones() {
+    fn test_count_ones_update() {
         let mut counts = vec![0,1,1,1,0];
         count_ones("00111".to_string(), & mut counts);
-        assert_eq!(counts[0], 0);
-        assert_eq!(counts[1], 1);
-        assert_eq!(counts[2], 2);
-        assert_eq!(counts[3], 2);
-        assert_eq!(counts[4], 1);
+        assert_eq!(counts, [0,1,2,2,1]);
+    }
+
+    #[test]
+    fn test_count_ones_initial() {
+        let mut counts = Vec::new();
+        count_ones("00111".to_string(), & mut counts);
+        assert_eq!(counts, vec![0,0,1,1,1])
     }
 
     #[test]
@@ -92,8 +107,21 @@ mod tests {
     }
 
     #[test]
-    fn test_beta() {
-        let b = beta(&vec![0,0,1,1,1]);
+    fn test_epsilon() {
+        let b = epsilon(&vec![0,0,1,1,1]);
         assert_eq!(b, vec![1,1,0,0,0])
+    }
+
+    #[test]
+    fn test_bit_vec_to_int() {
+        let v: Vec<i8> = vec![1,0,1];
+        let s = bit_vec_to_int(&v);
+        assert_eq!(5, s);
+        let v: Vec<i8> = vec![1,0,0];
+        let s = bit_vec_to_int(&v);
+        assert_eq!(4, s);
+        let v: Vec<i8> = vec![1,1,1];
+        let s = bit_vec_to_int(&v);
+        assert_eq!(7, s)
     }
 }
