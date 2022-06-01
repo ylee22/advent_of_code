6577d6d1d3d00740b1d462e4307d67da4eee4040
Yerim Lee <rimmy2002@gmail.com>
2022-06-01 15:35:52 -0700

    finished writing unittests for all of the functions in the aoc4 python program

--- day4/python/aoc4.py
+++ day4/python/aoc4.py
@@ -8,42 +8,94 @@ def parse_board_line(line):
     return list(map(int, line.split()))
 
 def construct_board(lines):
+    """ Construct a single board given an iterable of lines
+
+    Args:
+        lines (iterable): returns a board line one at a time
+
+    Returns:
+        np.array: nxn numpy array representing the bingo board
+    """
     board = []
-    for _ in range(5):
-        bl = parse_board_line(lines.readline())  
+    for l in lines:
+        bl = parse_board_line(l)  
         board.append(bl)
-    
     return np.array(board)
 
-def bingo(board, nums):
+def bingo(board, nums, n):
+    """ Return whether there is a bingo for a single board
+
+    Args:
+        board (np.array): numpy array representing a nxn board of ints
+        nums (list): a list of numbers drawn to check for a bingo
+        n (int): size of the board (nxn)
+
+    Returns:
+        option: returns an int if a bingo was found, None if there was no bingo
+    """
     rows = []
     cols = []
-    for n in nums:
-        (r, c) = np.where(board==n)
-        rows.append(r)
-        cols.append(c)
-    
-        if len(rows) == 5 or len(cols) == 5:
-            s = score(board, rows, cols)
-            return s
-        else:
-            return False
+    for num in nums:
+        (r, c) = np.where(board==num)
+        rows.append(r[0])
+        cols.append(c[0])
 
-def score(board, r_idx, c_idx):
+        if len(rows) == n or len(cols) == n:
+            s = score(board, rows, cols, num)
+            return s
+
+    return None
+
+def score(board, r_idx, c_idx, num):
+    """ Return the score of a board given that a bingo was found
+
+    Args:
+        board (np.array): nxn numpy array of ints representing the board
+        r_idx (list): list of row indices of the board numbers drawn
+        c_idx (list): list of column indices of the board numbers drawn
+        num (_type_): the last number drawn for bingo
+
+    Returns:
+        int: the score of the board calculated as the sum of all the numbers
+             that were NOT drawn on the board multiplied by the last number drawn
+    """
     r_idx = np.array(r_idx)
     c_idx = np.array(c_idx)
     num_drawn = board[r_idx, c_idx].sum()
-    return board.sum().sum() - num_drawn
+    return (board.sum().sum() - num_drawn)*num
+
+def take(n, l):
+    """ Take the first n elements from an iterable l.
+    Importantly, this mutates l so that the position in the iterable is not lost.
+
+    Args:
+        n (int): the number of elements to take from l
+        l (iterable): an iterable that can return elements one at a time
+
+    Returns:
+        list: a list of n first elements from l
+    """
+    output = []
+    for i, e in enumerate(l):
+        output.append(e)
+        if i == n-1:
+            break
+    return output
 
 def main():
     lines = sys.stdin
     nums = parse_num(lines.readline())
 
     boards = []
+    board_length = 5
     # filter for empty lines
     lines = filter(lambda x: x, lines)
-    while lines:
-        board = construct_board(lines)
+    while True:
+        l = take(board_length, lines)
+        if len(l) < board_length:
+            break
+
+        board = construct_board(l)
         boards.append(board)
-    bingo(board, nums)
+    bingo(board, nums, board_length)
     
--- day4/python/test.py
+++ day4/python/test.py
@@ -1,37 +1,61 @@
 import unittest
-from aoc4 import parse_num, parse_board_line, construct_board
+import numpy as np
+from aoc4 import parse_num, parse_board_line, take, construct_board, bingo, score
 
 
-class Test(unittest.TestCase):
-    def test_everything(self):
+class TestParse(unittest.TestCase):
+    def test_parse_num(self):
         num = parse_num("7,4,9,5,11,17,23,2,0")
         self.assertEqual([7,4,9,5,11,17,23,2,0], num)
+
+    def test_parse_board_line(self):
         board_line = parse_board_line("22 13 17 11  0")
         self.assertEqual([22,13,17,11,0], board_line)
 
-        lines = ["",
+    def test_take(self):
+        self.assertEqual(take(5, [1,2,3,4]), [1,2,3,4])
+        self.assertEqual(take(3, [1,2,3,4]), [1,2,3])
+
+    def test_take_iter(self):
+        """ testing that the iterator state is mutated with take and that the index is not reset
+        """
+        my_iter = iter([1,2,3,4,5,6,7])
+        self.assertEqual(take(3, my_iter), [1,2,3])
+        self.assertEqual(take(2, my_iter), [4,5])
+        self.assertEqual(take(2, my_iter), [6,7])
+
+    def test_construct_board(self):
+        lines = [
                 "68 73 98 51 49",
                 "82 56 87 64  8",
                 "46  7 21 38 30",
                 "66  5 86 97 74",
                 "60 63 76 55 39",
-                "",
-                "92 20 87 77 52",
-                "72 29 81 24 64",
-                "26 16 19 79 68",
-                "8 53 90 14 74",
-                "28 89 78 54 15",
-                "",
-                "13 17 35  2 85",
-                "37 87 57 74 65",
-                "60 21 18 98 96",
-                "4 51 46 84  0",
-                "90 75 80 41 64",
-                ""]
+                ]
 
         board = construct_board(lines)
-        self.assertEqual(board[0][2,3], 38)
-    
+        self.assertEqual(board[2,3], 38)
+        self.assertEqual(board[4,0], 60)
+
+class TestBingo(unittest.TestCase):
+    def test_score(self):
+        board = np.array([
+                [0,1,2],
+                [3,4,5],
+                [6,7,8]
+                ])
+        rows = [0,0,0]
+        cols = [0,1,2]
+        s = score(board, rows, cols, 2)
+        self.assertEqual(s, 66)
+
+    def test_bingo(self):
+        board = np.array([
+                        [0,1,2],
+                        [3,4,5],
+                        [6,7,8]
+                        ])
+        self.assertEqual(bingo(board, [3,4,5], 3), 120)
 
 if __name__ == '__main__':
     unittest.main()
